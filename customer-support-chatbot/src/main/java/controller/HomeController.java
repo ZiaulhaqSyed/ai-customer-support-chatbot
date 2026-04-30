0b31f1b6f1445cd8e43564d62d848d307ea3b134
saiyyedtech07 <saiyyed.tech07@gmail.com>
2026-04-30 22:59:03 +0530

    Commit 1: Initialize project and configure Git setup from feature branch

--- customer-support-chatbot/src/main/java/controller/HomeController.java
+++ customer-support-chatbot/src/main/java/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/home")
     public String home() {
-        System.out.println("Home page accessed");
+        System.out.println(" This is feature branch ");
         return "home"; // This will render the home.html template
     }
 
